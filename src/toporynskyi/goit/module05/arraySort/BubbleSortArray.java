ffadc1e8208ebe4a286e838fc3d69ffeaf8fb52c
Vasyl Toporynskyi <basileuse@gmail.com>
2016-06-08 17:45:32 +0300

    3d commit

--- src/toporynskyi/goit/module05/arraySort/BubbleSortArray.java
+++ src/toporynskyi/goit/module05/arraySort/BubbleSortArray.java
@@ -7,22 +7,9 @@ import java.util.Random;
  */
 public class BubbleSortArray {
 
-    public static void main(String[] args) {
+    public static void bubbleSort(int[] array){
 
-        int [] array = new int[20];
-        Random randomAdd = new Random();
-
-        System.out.print("Random Array: \n");
-
-        for(int i = 0; i < array.length; i++){
-            array[i] = randomAdd.nextInt(100);
-          System.out.print(array[i] + "  ");
-        }
-
-        System.out.print("\n");
-        System.out.print("Bubble sort result: \n");
-
-        int min = 0;
+        int min;
         int tmp;
 
         for(int i=0; i<array.length; i++)
@@ -35,7 +22,28 @@ public class BubbleSortArray {
             tmp = array[i];
             array[i] = array[min];
             array[min] = tmp;
-            System.out.print(array[i] + "  ");
         }
+    }
+
+    public static void showResults(int[] array){
+        for(int i=0; i<array.length; i++)
+            System.out.print(array[i] + "  ");
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int [] array = new int[20];
+        Random randomAdd = new Random();
+        System.out.print(" Random Array: \n" );
+        for(int i = 0; i < array.length; i++){
+            array[i] = randomAdd.nextInt(100);
+          System.out.print(array[i] + "  ");
+        }
+
+        BubbleSortArray.bubbleSort(array);
+        System.out.println("\n" + " Sorted Array");
+        BubbleSortArray.showResults(array);
     }
 }
--- src/toporynskyi/goit/module05/maxminArray/FindMaxMin.java
+++ src/toporynskyi/goit/module05/maxminArray/FindMaxMin.java
@@ -7,28 +7,42 @@ import java.util.Random;
  */
 public class FindMaxMin {
 
+    public static int findMaxElement(int[] array) {
+
+        int maxElement = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (maxElement < array[i])
+                maxElement = array[i];
+        }
+        return maxElement;
+    }
+
+    public static int findMinElement(int[] array){
+
+        int minElement = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (minElement > array[i])
+                minElement = array[i];
+        }
+        return minElement;
+    }
+
+
     public static void main(String[] args) {
 
-        int [] array = new int[20];
+        int[] array = new int[20];
         Random randomAdd = new Random();
 
-        for(int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = randomAdd.nextInt(100);
             System.out.print(array[i] + " | ");
         }
+        System.out.println("\n");
+        System.out.println("Min = " + FindMaxMin.findMinElement(array) + "\n");
+        System.out.println("Max = " + FindMaxMin.findMaxElement(array) + "\n");
 
-        int maxElement = array[0];
-        for(int i = 0; i < array.length; i++){
-            if(maxElement < array[i])
-                maxElement = array[i];
-        }
-
-        int minElement = array[0];
-        for(int i = 0; i < array.length; i++){
-            if(minElement > array[i])
-                minElement = array[i];
-        }
-
-        System.out.print("\n MaxElement = " + maxElement + "\n MinElement = " + minElement);
     }
+
+
+
 }
