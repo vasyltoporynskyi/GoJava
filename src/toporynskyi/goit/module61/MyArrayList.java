de8af420e1c08fb7d6a21c253fa542a2074ed5be
Vasyl Toporynskyi <basileuse@gmail.com>
2016-07-06 16:33:44 +0300

    Commit with MyArray class.

---
+++ src/toporynskyi/goit/module61/MyArrayList.java
@@ -0,0 +1,28 @@
+package toporynskyi.goit.module61;
+
+import java.util.*;
+
+/**
+ * Created by grant on 7/6/16.
+ */
+public class MyArrayList {
+    List<Integer> myArray = new ArrayList<>();
+
+
+    public void add(){
+
+    }
+
+    public void get(int index){
+
+    }
+
+    public void size(){
+
+    }
+
+    public void isEmpty(){
+
+    }
+
+}
