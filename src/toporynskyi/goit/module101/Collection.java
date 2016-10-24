8b0242f98238c26f55ddc2c82b645d829df795cd
Vasyl Toporynskyi <basileuse@gmail.com>
2016-10-24 15:38:10 +0300

    Add module101 task.

---
+++ src/toporynskyi/goit/module101/Collection.java
@@ -0,0 +1,7 @@
+package toporynskyi.goit.module101;
+
+/**
+ * Created by grant on 10/24/16.
+ */
+public class Collection {
+}
---
+++ src/toporynskyi/goit/module101/CollectionAddElement.java
@@ -0,0 +1,8 @@
+package toporynskyi.goit.module101;
+
+/**
+ * Created by grant on 10/24/16.
+ */
+public class CollectionAddElement {
+
+}
