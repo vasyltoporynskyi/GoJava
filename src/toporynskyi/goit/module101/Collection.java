7088bd5855d57b2732d46f44b61f237518b732d8
Vasyl Toporynskyi <basileuse@gmail.com>
2016-10-24 17:07:28 +0300

    Commit task101

--- src/toporynskyi/goit/module101/Collection.java
+++ src/toporynskyi/goit/module101/Collection.java
@@ -1,7 +1,12 @@
 package toporynskyi.goit.module101;
 
+import java.util.*;
+
 /**
  * Created by grant on 10/24/16.
  */
 public class Collection {
+
+    protected List<Integer> list = new ArrayList<>();
+
 }
--- src/toporynskyi/goit/module101/CollectionAddElement.java
+++ src/toporynskyi/goit/module101/CollectionAddElement.java
@@ -1,8 +1,55 @@
 package toporynskyi.goit.module101;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by grant on 10/24/16.
  */
 public class CollectionAddElement {
 
-}
+    public static void main(String [] args){
+
+        List<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+ //       Iterator<Integer> iterator = list.iterator();
+
+        for (int n: list) {
+            System.out.println(n + " ");
+        }
+
+        System.out.print("++++++++++++++++++++++" + "\n");
+
+        list.add(5, 6);
+
+        for (int n: list) {
+            System.out.println(n + " ");
+        }
+        System.out.println("++++++++++++++++++++++" + "\n");
+
+        int element = list.get(5);
+
+ //       Iterator<Integer> iterator = list.iterator();
+ //       while (iterator.hasNext()) {
+            for(int i = 0; i < list.size() - 1; i++ ){
+                    list.add(element);
+
+                }
+
+
+
+        for (int n: list) {
+            System.out.println(n + " ");
+        }
+        System.out.println("++++++++++++++++++++++" + "\n");
+
+       }
+    }
+
